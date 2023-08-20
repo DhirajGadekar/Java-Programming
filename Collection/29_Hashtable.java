c1c10ac26c332804cc733a4a7425bc1600a8796c
dhirajgadekar <dhirajgadekar629@gmail.com>
2023-08-20 12:28:54 +0530

    Hashtable

---
+++ Collection/29_Hashtable.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class HashtableDemo {
+	
+	public static void main(String[] args) {
+		
+		Hashtable map = new Hashtable();
+
+		map.put(10, "Sachin");
+		map.put(7, "MSD");
+		map.put(45, "Rohit");
+		map.put(18, "Virat");
+		map.put(1, "KLRahul");
+		
+		System.out.println(map);
+
+	}
+}
